01a045adcfb6c98e33e35521b1549ce08be1e4be
shiroemons <shiroemons@gmail.com>
2021-07-27 00:54:18 +0900

    Add Spotify artists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_133757) do
+ActiveRecord::Schema.define(version: 2021_07_26_143132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -49,6 +49,16 @@ ActiveRecord::Schema.define(version: 2021_07_26_133757) do
     t.string "short_title", null: false
     t.string "original_type", null: false
     t.float "series_order", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "spotify_artists", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "spotify_id", null: false
+    t.string "name", null: false
+    t.string "url"
+    t.integer "follower_count"
+    t.jsonb "payload"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
