e65760a32c2c52ae91443ec3d427d9eb12b09df7
shiroemons <shiroemons@gmail.com>
2021-07-27 09:40:42 +0900

    Add save_spotify_artist method

--- app/models/spotify_client/artist.rb
+++ app/models/spotify_client/artist.rb
@@ -4,11 +4,17 @@ module SpotifyClient
   class Artist
     def self.fetch(id)
       artist = RSpotify::Artist.find(id)
-      spotify_id = artist.id
-      name = artist.name
-      url = artist.external_urls['spotify']
+      save_spotify_artist(artist)
+    end
+
+    def self.save_spotify_artist(artist)
+      spotify_artist = SpotifyArtist.find_or_create_by!(
+        spotify_id: artist.id,
+        name: artist.name,
+        url: artist.external_urls['spotify']
+      )
+
       follower_count = artist.followers['total']
-      spotify_artist = SpotifyArtist.find_or_create_by!(spotify_id: spotify_id, name: name, url: url)
       spotify_artist.update!(follower_count: follower_count, payload: artist.as_json) if spotify_artist.follower_count != follower_count
     end
   end
