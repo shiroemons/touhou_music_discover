4c441123618c6ed9794df921ea4c52fbf29b1216
shiroemons <shiroemons@gmail.com>
2021-07-29 10:50:31 +0900

    Add association

--- app/models/apple_music_album.rb
+++ app/models/apple_music_album.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class AppleMusicAlbum < ApplicationRecord
+  has_many :apple_music_tracks,
+           -> { order(Arel.sql('apple_music_tracks.track_number ASC')) },
+           inverse_of: :apple_music_album,
+           dependent: :destroy
+
+  belongs_to :album, optional: true
+
+  delegate :jan_code, to: :album, allow_nil: true
 end
--- app/models/apple_music_track.rb
+++ app/models/apple_music_track.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class AppleMusicTrack < ApplicationRecord
+  belongs_to :apple_music_album
+  belongs_to :track
+
+  delegate :isrc, to: :track, allow_nil: true
 end
