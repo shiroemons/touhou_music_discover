b163328a076a305dc4b5880ba4077193174e24b8
shiroemons <shiroemons@gmail.com>
2021-08-01 02:33:08 +0900

    Add spotify_id scope

--- app/models/spotify_track.rb
+++ app/models/spotify_track.rb
@@ -6,4 +6,6 @@ class SpotifyTrack < ApplicationRecord
   belongs_to :track
 
   delegate :isrc, :is_touhou, to: :track, allow_nil: true
+
+  scope :spotify_id, ->(spotify_id) { find_by(spotify_id: spotify_id) }
 end
