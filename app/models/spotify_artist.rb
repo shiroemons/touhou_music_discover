27296eec22dab08c7b22363e3280a6f0e7a3c00a
shiroemons <shiroemons@gmail.com>
2021-08-01 16:18:56 +0900

    Move save_spotify_artist to SpotifyArtist and change it to save_artist

--- app/models/spotify_artist.rb
+++ app/models/spotify_artist.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class SpotifyArtist < ApplicationRecord
+  def self.save_artist(s_artist)
+    return nil if s_artist.blank?
+
+    spotify_artist = SpotifyArtist.find_or_create_by!(
+      spotify_id: s_artist.id,
+      name: s_artist.name,
+      url: s_artist.external_urls['spotify']
+    )
+
+    spotify_artist.update!(
+      follower_count: s_artist.followers['total'],
+      payload: s_artist.as_json
+    )
+    spotify_artist
+  end
 end
--- app/models/spotify_client/artist.rb
+++ app/models/spotify_client/artist.rb
@@ -3,19 +3,8 @@
 module SpotifyClient
   class Artist
     def self.fetch(id)
-      artist = RSpotify::Artist.find(id)
-      save_spotify_artist(artist)
-    end
-
-    def self.save_spotify_artist(artist)
-      spotify_artist = SpotifyArtist.find_or_create_by!(
-        spotify_id: artist.id,
-        name: artist.name,
-        url: artist.external_urls['spotify']
-      )
-
-      follower_count = artist.followers['total']
-      spotify_artist.update!(follower_count: follower_count, payload: artist.as_json) if spotify_artist.follower_count != follower_count
+      s_artist = RSpotify::Artist.find(id)
+      SpotifyArtist.save_artist(s_artist)
     end
   end
 end
