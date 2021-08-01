5e7ff8c5934809c528a35c928f3d68d430d28e8b
shiroemons <shiroemons@gmail.com>
2021-08-01 15:51:39 +0900

    Move save_apple_music_artist to AppleMusicArtist and change it to save_artist

--- app/models/apple_music_artist.rb
+++ app/models/apple_music_artist.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class AppleMusicArtist < ApplicationRecord
+  def self.save_artist(am_artist)
+    apple_music_artist = AppleMusicArtist.find_or_create_by!(
+      apple_music_id: am_artist.id,
+      name: am_artist.name,
+      url: am_artist.url
+    )
+
+    apple_music_artist.update!(payload: am_artist.as_json)
+  end
 end
--- app/models/apple_music_client/artist.rb
+++ app/models/apple_music_client/artist.rb
@@ -4,17 +4,7 @@ module AppleMusicClient
   class Artist
     def self.fetch(id)
       artist = AppleMusic::Artist.find(id)
-      save_apple_music_artist(artist)
-    end
-
-    def self.save_apple_music_artist(artist)
-      apple_music_artist = AppleMusicArtist.find_or_create_by!(
-        apple_music_id: artist.id,
-        name: artist.name,
-        url: artist.url
-      )
-
-      apple_music_artist.update!(payload: artist.as_json) if apple_music_artist.payload.nil?
+      AppleMusicArtist.save_artist(artist)
     end
   end
 end
