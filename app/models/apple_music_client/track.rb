129f26da33afcaf025b66054e19f6b44787e2eca
shiroemons <shiroemons@gmail.com>
2021-08-01 20:01:46 +0900

    Create the fetch_tracks method

--- app/models/apple_music_client/track.rb
+++ app/models/apple_music_client/track.rb
@@ -7,19 +7,23 @@ module AppleMusicClient
     def self.fetch_album_tracks(apple_music_album)
       return if AppleMusicTrack.where(apple_music_album_id: apple_music_album.id).count == apple_music_album.total_tracks
 
+      am_tracks = fetch_tracks(apple_music_album.apple_music_id)
+      am_tracks.each do |am_track|
+        AppleMusicTrack.save_track(apple_music_album, am_track)
+      end
+    end
+
+    def self.fetch_tracks(album_id)
       am_tracks = []
       offset = 0
       loop do
-        tracks = AppleMusic::Album.get_relationship(apple_music_album.apple_music_id, :tracks, limit: LIMIT, offset: offset)
+        tracks = AppleMusic::Album.get_relationship(album_id, :tracks, limit: LIMIT, offset: offset)
         am_tracks.push(*tracks)
         break if tracks.size < LIMIT
 
         offset += LIMIT
       end
-
-      am_tracks.each do |am_track|
-        AppleMusicTrack.save_track(apple_music_album, am_track)
-      end
+      am_tracks
     end
   end
 end
