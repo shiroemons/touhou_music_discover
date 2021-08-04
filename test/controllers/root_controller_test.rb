76637f7e0f2bcb382537986952fb8e1bbe564028
shiroemons <shiroemons@gmail.com>
2021-08-05 00:51:54 +0900

    rails g controller root index

---
+++ test/controllers/root_controller_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class RootControllerTest < ActionDispatch::IntegrationTest
+  test 'should get index' do
+    get root_index_url
+    assert_response :success
+  end
+end
