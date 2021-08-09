682b3d06bc265c70a3c51e669f7722304afcccb8
shiroemons <shiroemons@gmail.com>
2021-08-09 19:11:44 +0900

    Add spotify playlist

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  namespace :spotify do
+    post 'playlists/create'
+  end
 end
