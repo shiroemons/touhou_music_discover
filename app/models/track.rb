ff176daa7e25a355a7d898810620cc707b23e0bb
shiroemons <shiroemons@gmail.com>
2022-04-20 23:45:13 +0900

    Add ytmusic_tracks

--- app/models/track.rb
+++ app/models/track.rb
@@ -8,12 +8,14 @@ class Track < ApplicationRecord
   has_many :line_music_tracks, dependent: :destroy
   has_many :spotify_tracks, dependent: :destroy
   has_many :spotify_track_audio_features, dependent: :destroy
+  has_many :ytmusic_tracks, dependent: :destroy
 
   belongs_to :album, foreign_key: :jan_code, primary_key: :jan_code, inverse_of: :tracks
 
   scope :missing_apple_music_tracks, -> { where.missing(:apple_music_tracks) }
   scope :missing_line_music_tracks, -> { where.missing(:line_music_tracks) }
   scope :missing_spotify_tracks, -> { where.missing(:spotify_tracks) }
+  scope :missing_ytmusic_tracks, -> { where.missing(:ytmusic_tracks) }
   scope :missing_original_songs, -> { where.missing(:original_songs) }
   scope :is_touhou, -> { where(is_touhou: true) }
   scope :non_touhou, -> { where(is_touhou: false) }
@@ -30,5 +32,9 @@ class Track < ApplicationRecord
 
   def spotify_track(album)
     spotify_tracks.find{_1.album == album}
+  end
+
+  def ytmusic_track(album)
+    ytmusic_tracks.find{_1.album == album}
   end
 end
