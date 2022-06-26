99477686c9f4c8b933759e14d93e61e3eb2434a8
shiroemons <shiroemons@gmail.com>
2022-06-26 19:21:01 +0900

    Add master_artist_resource

---
+++ app/avo/resources/master_artist_resource.rb
@@ -0,0 +1,18 @@
+class MasterArtistResource < Avo::BaseResource
+  self.title = :name
+  self.description = 'アーティストのマスターデータ'
+  self.includes = []
+  self.record_selector = false
+  self.search_query = ->(params:) do
+    scope.ransack(name_cont: params[:q], key_eq: params[:q], m: "or").result(distinct: false)
+  end
+
+  field :id, as: :id, hide_on: [:index]
+  field :name, as: :text
+  field :complex_name, as: :text, hide_on: :all, as_label: true do |model|
+    "[#{model.streaming_type}][#{model.name}] #{model.key}"
+  end
+  field :key, as: :text
+  field :streaming_type, as: :select, enum: ::MasterArtist.streaming_types, hide_on: [:show, :index]
+  field :streaming_type, as: :badge, options: { success: 'spotify', danger: 'apple_music' }
+end
---
+++ app/controllers/avo/master_artists_controller.rb
@@ -0,0 +1,2 @@
+class Avo::MasterArtistsController < Avo::ResourcesController
+end
