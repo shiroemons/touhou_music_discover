ff1af6f314a9a60f6c0f82e70ea2623b9c1ae2c6
shiroemons <shiroemons@gmail.com>
2024-03-28 23:44:11 +0900

    Apple Musicの情報を先に参照するように修正

--- app/avo/actions/fetch_ytmusic_track.rb
+++ app/avo/actions/fetch_ytmusic_track.rb
@@ -13,6 +13,19 @@ class FetchYtmusicTrack < Avo::BaseAction
       next if ytm_album.total_tracks == ytm_album.ytmusic_tracks.size
 
       ytm_tracks = ytm_album.payload&.dig('tracks')
+
+      # Apple Musicは、YouTube Musicと同様にtrack_numberが通し番号となっているため、先にApple Musicをもとに処理をする
+      am_album = album.apple_music_album
+      if am_album.present?
+        am_album.apple_music_tracks.each do |am_track|
+          ytm_track = ytm_tracks.find { _1['track_number'] == am_track.track_number }
+          next if ytm_track.nil?
+
+          YtmusicTrack.save_track(album.id, am_track.track_id, ytm_album, ytm_track)
+        end
+        next
+      end
+
       s_album = album.spotify_album
       if s_album.present?
         s_album.spotify_tracks.each do |s_track|
@@ -22,16 +35,6 @@ class FetchYtmusicTrack < Avo::BaseAction
           YtmusicTrack.save_track(album.id, s_track.track_id, ytm_album, ytm_track)
         end
         next
-      end
-
-      am_album = album.apple_music_album
-      next if am_album.blank?
-
-      am_album.apple_music_tracks.each do |am_track|
-        ytm_track = ytm_tracks.find { _1['track_number'] == am_track.track_number }
-        next if ytm_track.nil?
-
-        YtmusicTrack.save_track(album.id, am_track.track_id, ytm_album, ytm_track)
       end
     end
 
