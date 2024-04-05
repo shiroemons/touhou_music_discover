635d01702cb43b4be96f72c436d8db8230a07726
shiroemons <shiroemons@gmail.com>
2024-04-06 00:07:08 +0900

    ytmusic track取得処理を並列処理化

--- app/avo/actions/fetch_ytmusic_track.rb
+++ app/avo/actions/fetch_ytmusic_track.rb
@@ -6,39 +6,49 @@ class FetchYtmusicTrack < Avo::BaseAction
   self.visible = -> { view == :index }
 
   def handle(_args)
-    Album.includes(:ytmusic_album, spotify_album: [:spotify_tracks], apple_music_album: [:apple_music_tracks]).find_each do |album|
-      ytm_album = album.ytmusic_album
-      next if ytm_album.blank?
-
-      next if ytm_album.total_tracks == ytm_album.ytmusic_tracks.size
-
-      ytm_tracks = ytm_album.payload&.dig('tracks')
-
-      # Apple Musicは、YouTube Musicと同様にtrack_numberが通し番号となっているため、先にApple Musicをもとに処理をする
-      am_album = album.apple_music_album
-      if am_album.present?
-        am_album.apple_music_tracks.each do |am_track|
-          ytm_track = ytm_tracks.find { _1['track_number'] == am_track.track_number }
-          next if ytm_track.nil?
-
-          YtmusicTrack.save_track(album.id, am_track.track_id, ytm_album, ytm_track)
+    album_ids = Album.pluck(:id)
+    batch_size = 1000
+    album_ids.each_slice(batch_size) do |ids|
+      Album.includes(:ytmusic_album, spotify_album: [:spotify_tracks], apple_music_album: [:apple_music_tracks]).where(id: ids).then do |records|
+        Parallel.each_with_index(records, in_processes: 7) do |r|
+          process_album(r)
         end
-        next
-      end
-
-      s_album = album.spotify_album
-      if s_album.present?
-        s_album.spotify_tracks.each do |s_track|
-          ytm_track = ytm_tracks.find { _1['track_number'] == s_track.track_number }
-          next if ytm_track.nil?
-
-          YtmusicTrack.save_track(album.id, s_track.track_id, ytm_album, ytm_track)
-        end
-        next
       end
     end
 
     succeed 'Done!'
     reload
+  end
+
+  def process_album(album)
+    ytm_album = album.ytmusic_album
+    return if ytm_album.blank? || ytm_album.total_tracks == ytm_album.ytmusic_tracks.size
+
+    ytm_tracks = ytm_album.payload&.dig('tracks')
+    return if ytm_tracks.blank?
+
+    if album.apple_music_album.present?
+      process_am_tracks(album, ytm_album, ytm_tracks)
+    elsif album.spotify_album.present?
+      process_sp_tracks(album, ytm_album, ytm_tracks)
+    end
+  end
+
+  def process_am_tracks(album, ytm_album, ytm_tracks)
+    album.apple_music_album.apple_music_tracks.each do |am_track|
+      ytm_track = ytm_tracks.find { _1['track_number'] == am_track.track_number }
+      next if ytm_track.nil?
+
+      YtmusicTrack.save_track(album.id, am_track.track_id, ytm_album, ytm_track)
+    end
+  end
+
+  def process_sp_tracks(album, ytm_album, ytm_tracks)
+    album.spotify_album.spotify_tracks.each do |s_track|
+      ytm_track = ytm_tracks.find { _1['track_number'] == s_track.track_number }
+      next if ytm_track.nil?
+
+      YtmusicTrack.save_track(album.id, s_track.track_id, ytm_album, ytm_track)
+    end
   end
 end
