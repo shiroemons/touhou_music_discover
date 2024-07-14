74f3665266436ec2b86be013f5096ed3501f2c06
shiroemons <shiroemons@gmail.com>
2024-07-14 13:57:38 +0900

    YouTube Musicのデータ構造を修正

--- lib/ytmusic/album.rb
+++ lib/ytmusic/album.rb
@@ -20,21 +20,23 @@ module YTMusic
                 :tracks, :duration_seconds
 
     def initialize(response)
-      @title = response.dig('header', 'musicDetailHeaderRenderer', 'title', 'runs', 0, 'text')
-      contents = response.dig('header', 'musicDetailHeaderRenderer', 'subtitle', 'runs')
-      @type = contents&.shift&.dig('text')
-      @year = contents&.pop&.dig('text')
-      artist_contents = contents&.filter { _1['text'] != ' • ' }&.filter { _1['text'] != '、' }
+      header = response.dig('contents', 'twoColumnBrowseResultsRenderer', 'tabs', 0, 'tabRenderer', 'content', 'sectionListRenderer', 'contents', 0, 'musicResponsiveHeaderRenderer')
+      @title = header.dig('title', 'runs', 0, 'text')
+      subtitle = header.dig('subtitle', 'runs')
+      @type = subtitle&.shift&.dig('text')
+      @year = subtitle&.pop&.dig('text')
+      strapline_text_one = header.dig('straplineTextOne', 'runs')
+      artist_contents = strapline_text_one&.filter { _1['text'] != ' • ' }&.filter { _1['text'] != '、' }
       return if artist_contents.blank?
 
       @artists = artist_contents.map { Artist.new _1 }
 
-      @track_total_count = response.dig('header', 'musicDetailHeaderRenderer', 'secondSubtitle', 'runs', 0, 'text').to_i
-      @duration_text = response.dig('header', 'musicDetailHeaderRenderer', 'secondSubtitle', 'runs', 2, 'text')
-      thumbnails = response.dig('header', 'musicDetailHeaderRenderer', 'thumbnail', 'croppedSquareThumbnailRenderer', 'thumbnail', 'thumbnails')
+      @track_total_count = header.dig('secondSubtitle', 'runs', 0, 'text').to_i
+      @duration_text = header.dig('secondSubtitle', 'runs', 2, 'text')
+      thumbnails = header.dig('straplineThumbnail', 'musicThumbnailRenderer', 'thumbnail', 'thumbnails')
       @thumbnails = thumbnails.map { Thumbnail.new _1 }
       @playlist_url = response.dig('microformat', 'microformatDataRenderer', 'urlCanonical')
-      track_contents = response.dig('contents', 'singleColumnBrowseResultsRenderer', 'tabs', 0, 'tabRenderer', 'content', 'sectionListRenderer', 'contents', 0, 'musicShelfRenderer', 'contents')
+      track_contents = response.dig('contents', 'twoColumnBrowseResultsRenderer', 'secondaryContents', 'sectionListRenderer', 'contents', 0, 'musicShelfRenderer', 'contents')
       @tracks = track_contents.map { Track.new _1 }
       @duration_seconds = @tracks.sum(&:duration_seconds)
       super()
