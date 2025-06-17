53cc2d27538aa1f2bbf405bf31d0fcd6e6a7463f
shiroemons <shiroemons@gmail.com>
2025-06-17 23:50:20 +0900

    meilisearch-rails v0.16.0の破壊的変更に対応
    - MeiliSearch::Railsモジュール名をMeilisearch::Railsに変更
    - v0.15.0以降の新しいモジュール名規則に準拠

--- config/initializers/meilisearch.rb
+++ config/initializers/meilisearch.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-MeiliSearch::Rails.configuration = {
+Meilisearch::Rails.configuration = {
   meilisearch_host: ENV.fetch('MEILISEARCH_HOST', nil),
   meilisearch_api_key: ENV.fetch('MEILISEARCH_API_KEY', nil)
 }
